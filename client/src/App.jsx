66c1abddd5075bbfb7991b5adae8b6fd6a1c0233
Oat Chana <chanathip0944729842@gmail.com>
2024-11-01 13:33:40 +0700

    feat:add context of user data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { createContext } from "react";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+
+export const UserDataContext = createContext();
 
 function App() {
   const userData = {
@@ -10,15 +13,21 @@ function App() {
     avatar: "https://placedog.net/100/100",
     level: "platinum",
   };
+  
 
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
+      <UserDataContext.Provider
+      value={{
+        user: userData
+      }}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </UserDataContext.Provider>
     </div>
   );
 }
