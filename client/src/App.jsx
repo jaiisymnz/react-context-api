3d49baf115435dc9a4674206f6d43ed7bcf065cb
Yurapon Tinlapat <you@example.com>
2024-11-01 15:18:38 +0700

    feat:add user props to page using context api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 import "./App.css";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+export const UserDataContext = React.createContext();
 
 function App() {
   const userData = {
@@ -12,14 +14,20 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <UserDataContext.Provider
+      value={{
+        user: userData,
+      }}
+    >
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </UserDataContext.Provider>
   );
 }
 
